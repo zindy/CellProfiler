981bd9b3d90cf70e08b1a93c2ba1576fce86c538
Allen Goodman <allen.goodman@icloud.com>
2016-03-24 16:58:11 -0400

    sentry integration

--- cellprofiler/cellprofilerapp.py
+++ cellprofiler/cellprofilerapp.py
@@ -1,70 +1,103 @@
-#!/usr/bin/env python
-# -*- coding: iso-8859-15 -*-
+# coding=utf-8
 
-import cStringIO
-import sys
-
-import wx
-
-import cellprofiler.preferences as cpp
-from cellprofiler.gui.errordialog import display_error_dialog
-from cellprofiler.icons import get_builtin_image
-# Make sure sys.excepthook is called for any uncaught exceptions, even in threads.
+import cellprofiler.gui.errordialog
+import cellprofiler.preferences
 import cellprofiler.utilities.thread_excepthook
+import cellprofiler.utilities.version
+import os
+import pip
+import platform
+import raven
+import raven.versioning
+import sys
+import wx
 
 cellprofiler.utilities.thread_excepthook.install_thread_sys_excepthook()
 
 
 class CellProfilerApp(wx.App):
+    def __excepthook__(self, exception, message, tracback):
+        def callback():
+            self.client.captureException(
+                exc_info=(exception, message, tracback)
+            )
+
+            cellprofiler.preferences.cancel_progress()
+
+            cellprofiler.gui.errordialog.display_error_dialog(self.frame, message, None, tb=tracback, continue_only=True, message="Exception in CellProfiler core processing")
+
+        wx.CallAfter(callback)
+
     def __init__(self, *args, **kwargs):
-        # allow suppression of version checking (primarily for nosetests).
-        self.check_for_new_version = kwargs.pop('check_for_new_version', False)
-        self.workspace_path = kwargs.pop('workspace_path', None)
-        self.pipeline_path = kwargs.pop('pipeline_path', None)
         self.abort_initialization = False
+
+        self.check_for_new_version = kwargs.pop('check_for_new_version', False)
+
+        path = os.path.join(os.path.dirname(__file__), os.pardir)
+
+        try:
+            release = raven.fetch_git_sha(path)
+        except raven.versioning.InvalidGitRepository:
+            release = raven.fetch_package_version("cellprofiler")
+
+        self.client = raven.Client(
+            dsn="https://3d53494dbaaf4e858afd79f56506a749:8a7a767a1924423f89c1fdfd69717fd5@app.getsentry.com/70887",
+            release=release,
+        )
+
+        self.client.user_context({
+            "installed_distributions": sorted(
+                [
+                    "{0:s}=={1:s}".format(
+                            distribution.key, distribution.version
+                    ) for distribution in pip.get_installed_distributions()
+                ]
+            ),
+            "machine": platform.machine(),
+            "processor": platform.processor(),
+            "python_implementation": platform.python_implementation(),
+            "python_version": platform.python_version(),
+            "release": platform.release(),
+            "system": platform.system(),
+            "version": platform.version(),
+        })
+
+        self.frame = None
+
+        self.pipeline_path = kwargs.pop('pipeline_path', None)
+
+        self.workspace_path = kwargs.pop('workspace_path', None)
+
         super(CellProfilerApp, self).__init__(*args, **kwargs)
 
     def OnInit(self):
-        # The wx.StandardPaths aren't available until this is set.
-        from cellprofiler.utilities.version import dotted_version
-        self.SetAppName('CellProfiler%s' % dotted_version)
+        import cellprofiler.gui.cpframe
 
-        from cellprofiler.gui.cpframe import CPFrame
-        self.frame = CPFrame(None, -1, "Cell Profiler")
-        try:
-            self.frame.start(self.workspace_path, self.pipeline_path)
-        except:
-            return 0
+        self.SetAppName(
+            u"CellProfiler{0:s}".format(
+                cellprofiler.utilities.version.dotted_version
+            )
+        )
+
+        self.frame = cellprofiler.gui.cpframe.CPFrame(None, -1, "CellProfiler")
+
+        self.frame.start(self.workspace_path, self.pipeline_path)
+
         if self.abort_initialization:
             return 0
 
-        # set up error dialog for uncaught exceptions
-        def show_errordialog(type, exc, tb):
-            def doit():
-                cpp.cancel_progress()
-                display_error_dialog(self.frame, exc, None, tb=tb, continue_only=True,
-                                     message="Exception in CellProfiler core processing")
-                # continue is really the only choice
+        sys.excepthook = self.__excepthook__
 
-            wx.CallAfter(doit)
-
-        # replace default hook with error dialog
-        self.orig_excepthook = sys.excepthook
-        sys.excepthook = show_errordialog
         self.SetTopWindow(self.frame)
+
         self.frame.Show()
+
         if self.frame.startup_blurb_frame.IsShownOnScreen():
             self.frame.startup_blurb_frame.Raise()
+
         return 1
 
     def OnExit(self):
-        from imagej.imagej2 import allow_quit
-        allow_quit()
-        # restore previous exception hook
-        sys.excepthook = self.orig_excepthook
+        import imagej.imagej2
 
-# end of class CellProfilerApp
-
-if __name__ == "__main__":
-    CellProfilerApp = CellProfilerApp(0)
-    CellProfilerApp.MainLoop()
+        imagej.imagej2.allow_quit()
